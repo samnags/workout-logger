d3039d40df34249cd6ee92b17f2b7a33ee0b2056
samnags <sam.nagourney@gmail.com>
2016-05-02 10:08:17 -0400

    Workout show page has sets

--- app/controllers/workout_sets_controller.rb
+++ app/controllers/workout_sets_controller.rb
@@ -7,7 +7,20 @@ class WorkoutSetsController < ApplicationController
 
   def create
     @workout = Workout.find(params[:workout_id])
-    @workout_set = @workout.workout_sets.build
+    @workout_set = @workout.workout_sets.build(workout_set_params)
+    if @workout_set.save
+      redirect_to @workout
+    else
+      render :new
+    end
   end
+
+
+  private 
+
+  def workout_set_params
+    params.require(:workout_set).permit(:exercise_id, :set_num, :weight, :reps)
+  end
+
 
 end
